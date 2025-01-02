33e383daa69ecf66d36984009fffc391787d0dcf
Austin Fahsl <austin@fahsl.io>
2025-01-02 16:11:06 -0700

    custom-plugins

--- libs/internal-plugin/src/generators/util-lib/generator.ts
+++ libs/internal-plugin/src/generators/util-lib/generator.ts
@@ -1,13 +1,17 @@
 import { formatFiles, Tree } from '@nx/devkit';
 import { libraryGenerator } from '@nx/js';
+import { join } from 'path';
 import { UtilLibGeneratorSchema } from './schema';
 
 export async function utilLibGenerator(
   tree: Tree,
   options: UtilLibGeneratorSchema
 ) {
+  const name = `util-${options.name}`;
+  const scope = options.name;
   await libraryGenerator(tree, {
-    directory: 'util-' + options.name,
+    directory: join(options.directory, name),
+    tags: `type:util, scope:${scope}`,
   });
   // comment the rest of the code
   await formatFiles(tree);
