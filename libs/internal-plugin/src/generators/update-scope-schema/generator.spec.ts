84e91b432746c1a1763239297237b9a79e031f87
Austin Fahsl <austin@fahsl.io>
2025-01-02 21:21:56 -0700

    complex generators

--- libs/internal-plugin/src/generators/update-scope-schema/generator.spec.ts
+++ libs/internal-plugin/src/generators/update-scope-schema/generator.spec.ts
@@ -1,20 +1,83 @@
+import { readJson, Tree } from '@nx/devkit';
 import { createTreeWithEmptyWorkspace } from '@nx/devkit/testing';
-import { Tree, readProjectConfiguration } from '@nx/devkit';
+import { libraryGenerator } from '@nx/js';
+import { generatorGenerator, pluginGenerator } from '@nx/plugin/generators';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
-import { updateScopeSchemaGenerator } from './generator';
-import { UpdateScopeSchemaGeneratorSchema } from './schema';
+import { Linter } from '@nx/eslint';
+import generator from './generator';
 
 describe('update-scope-schema generator', () => {
-  let tree: Tree;
-  const options: UpdateScopeSchemaGeneratorSchema = { name: 'test' };
+  let appTree: Tree;
 
-  beforeEach(() => {
-    tree = createTreeWithEmptyWorkspace();
+  beforeEach(async () => {
+    appTree = createTreeWithEmptyWorkspace();
+    await addUtilLibProject(appTree);
+    await libraryGenerator(appTree, {
+      name: 'foo',
+      tags: 'scope:foo',
+      directory: 'foo',
+    });
+    await libraryGenerator(appTree, {
+      name: 'bar',
+      tags: 'scope:bar',
+      directory: 'bar',
+    });
   });
 
-  it('should run successfully', async () => {
-    await updateScopeSchemaGenerator(tree, options);
-    const config = readProjectConfiguration(tree, 'test');
-    expect(config).toBeDefined();
+  it('should adjust the util-lib generator based on existing projects', async () => {
+    await generator(appTree);
+    const schemaJson = readJson(
+      appTree,
+      'libs/internal-plugin/src/generators/util-lib/schema.json'
+    );
+    expect(schemaJson.properties.directory['x-prompt'].items).toEqual([
+      {
+        value: 'foo',
+        label: 'foo',
+      },
+      {
+        value: 'bar',
+        label: 'bar',
+      },
+    ]);
+    const schemaInterface = appTree.read(
+      'libs/internal-plugin/src/generators/util-lib/schema.d.ts',
+      'utf-8'
+    );
+    expect(schemaInterface).toContain(`export interface UtilLibGeneratorSchema {
+  name: string;
+  directory: 'foo' | 'bar';
+}`);
   });
 });
+
+async function addUtilLibProject(tree: Tree) {
+  await pluginGenerator(tree, {
+    name: 'internal-plugin',
+    directory: 'libs/internal-plugin',
+    skipTsConfig: false,
+    unitTestRunner: 'jest',
+    linter: Linter.EsLint,
+    compiler: 'tsc',
+    skipFormat: false,
+    skipLintChecks: false,
+  });
+  await generatorGenerator(tree, {
+    name: 'util-lib',
+    path: 'libs/internal-plugin/src/generators/util-lib',
+    unitTestRunner: 'jest',
+  });
+  const filesToCopy = [
+    '../util-lib/generator.ts',
+    '../util-lib/schema.json',
+    '../util-lib/schema.d.ts',
+  ];
+  for (const file of filesToCopy) {
+    tree.write(
+      `libs/internal-plugin/src/generators/util-lib/${file}`,
+      readFileSync(join(__dirname, file))
+    );
+  }
+}
